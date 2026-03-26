d3505cda4d8b54f6080a4748e265b077a117c4dc
torrosyan99 <torosyan.ha@gmail.com>
2026-03-26 09:15:02 +0400

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,39 @@
+const menuButton = document.querySelector('.header__menu-button');
+const menu = document.querySelector('.menu');
+
+function toggleMenu() {
+  menu.classList.toggle('menu--active');
+  this.classList.toggle('header__menu-button--opened-menu')
+  document.body.classList.toggle('body-overflow')
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+
+
+const input = document.querySelector('.top__input')
+
+const updatePlaceholder = () => {
+  if (window.innerWidth < 768) {
+    input.placeholder = 'Поиск'
+  } else {
+    input.placeholder = 'Поиск: квартиры, авто, работа ...'
+  }
+}
+
+updatePlaceholder()
+window.addEventListener('resize', updatePlaceholder)
+
+
+new Swiper('.top__swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  navigation: {
+    nextEl: '.top__next',
+  }
+})
+
+new Swiper('.top__mobile-swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+})
