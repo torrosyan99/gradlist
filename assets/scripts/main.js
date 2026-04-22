63a3b1be092400b1b139a3ae173dbe0533dd5f62
torrosyan99 <torosyan.ha@gmail.com>
2026-04-22 12:04:24 +0400

    first commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,6 +25,7 @@ menuButton.addEventListener('click', toggleMenu)
 
 const input = document.querySelector('.top__input')
 
+if(input) {
 // обновление плейсхолдера в зависимости от ширины
 const updatePlaceholder = () => {
   if (window.innerWidth < 768) {
@@ -39,7 +40,7 @@ updatePlaceholder()
 
 // обновление при ресайзе
 window.addEventListener('resize', updatePlaceholder)
-
+}
 
 // ===== SWIPER INIT =====
 
@@ -52,15 +53,15 @@ new Swiper('.top__swiper', {
     prevEl: '.top__prev',
   },
   on: {
-    slideChange: function () {
-      const prevBtn = document.querySelector('.top__prev');
+      slideChange: function () {
+        const prevBtn = document.querySelector('.top__prev');
 
-      if (this.activeIndex > 0) {
-        prevBtn.classList.add('top__prev--active'); // добавляешь свой класс
-      } else {
-        prevBtn.classList.remove('top__prev--active');
+        if (this.activeIndex > 0) {
+          prevBtn.classList.add('top__prev--active'); // добавляешь свой класс
+        } else {
+          prevBtn.classList.remove('top__prev--active');
+        }
       }
-    }
   }
 })
 
@@ -75,48 +76,127 @@ new Swiper('.top__mobile-swiper', {
 
 // контейнер со всеми карточками
 const container = document.querySelector('.listings__items')
-
+if(container) {
 // текущие элементы (первичный рендер)
-const items = Array.from(container.querySelectorAll('.listings__item'))
+  const items = Array.from(container.querySelectorAll('.listings__item'))
 
-const BATCH_SIZE = 16 // сколько добавлять за раз
-let currentIndex = 0 // (заготовка под API)
-let isLoading = false // защита от повторных вызовов
+  const BATCH_SIZE = 16 // сколько добавлять за раз
+  let currentIndex = 0 // (заготовка под API)
+  let isLoading = false // защита от повторных вызовов
 
 // берём первые 16 как шаблон (будем клонировать)
-const sourceItems = items.slice(0, BATCH_SIZE)
+  const sourceItems = items.slice(0, BATCH_SIZE)
 
 // функция подгрузки следующей пачки
-function loadMore() {
-  // защита от двойного вызова
-  if (isLoading) return
-  isLoading = true
+  function loadMore() {
+    // защита от двойного вызова
+    if (isLoading) return
+    isLoading = true
 
-  // используем fragment для оптимизации (1 вставка в DOM)
-  const fragment = document.createDocumentFragment()
+    // используем fragment для оптимизации (1 вставка в DOM)
+    const fragment = document.createDocumentFragment()
 
-  for (let i = 0; i < BATCH_SIZE; i++) {
-    // клонируем элемент (true = с вложенными нодами)
-    const item = sourceItems[i % sourceItems.length].cloneNode(true)
-    fragment.appendChild(item)
+    for (let i = 0; i < BATCH_SIZE; i++) {
+      // клонируем элемент (true = с вложенными нодами)
+      const item = sourceItems[i % sourceItems.length].cloneNode(true)
+      fragment.appendChild(item)
+    }
+
+    // добавляем все элементы одной операцией
+    container.appendChild(fragment)
+
+    isLoading = false
   }
-
-  // добавляем все элементы одной операцией
-  container.appendChild(fragment)
-
-  isLoading = false
-}
 
 // проверка достижения низа страницы
-function handleScroll() {
-  const scrollBottom =
-    window.innerHeight + window.scrollY >= document.body.offsetHeight - 200
+  function handleScroll() {
+    const scrollBottom =
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 200
 
-  // если почти дошли до низа — грузим ещё
-  if (scrollBottom) {
-    loadMore()
+    // если почти дошли до низа — грузим ещё
+    if (scrollBottom) {
+      loadMore()
+    }
+  }
+
+// подписка на скролл
+  window.addEventListener('scroll', handleScroll)
+}
+
+
+const swiper = new Swiper(".product__swiper", {
+  spaceBetween: 15.5,
+  slidesPerView: 'auto',
+  freeMode: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".product__slider-next",
+    prevEl: ".product__slider-prev",
+  },
+  on: {
+    slideChange: function () {
+      const prevBtn = document.querySelector('.product__slider-prev');
+
+      if (this.activeIndex > 0) {
+        prevBtn.classList.add('product__slider-prev--active'); // добавляешь свой класс
+      } else {
+        prevBtn.classList.remove('product__slider-prev--active');
+      }
+    }
+  }
+});
+
+if(swiper) {
+  new Swiper(".product__main-swiper", {
+  loop: true,
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+}
+
+class Tabs {
+  constructor(tabsClass) {
+    const tabs = document.querySelector(tabsClass);
+    if(!tabs) return
+    this.data = {};
+    this.activeTab = null;
+    this.activeButton  = null;
+
+
+    const tabsButtons = tabs.querySelectorAll('.tabs__button');
+    if(tabsButtons.length > 0 ) {
+      tabsButtons[0].classList.add('tabs__button--active');
+      this.activeButton = tabsButtons[0]
+    }
+    const tabsItems = tabs.querySelectorAll('.tabs__item');
+    for(let i = 0; i < tabsItems.length; i++) {
+      const item = tabsItems[i];
+      if(i === 0) {
+        item.classList.add('tabs__item--active');
+        this.activeTab = item;
+      }
+      this.data[item.dataset.index] = item;
+    }
+
+    tabs.addEventListener('click', (e) => {
+       const target = e.target;
+       const button = target.closest('.tabs__button')
+      if(button && button !== this.activeButton ) {
+        this.activeButton.classList.remove('tabs__button--active');
+        this.activeTab.classList.remove('tabs__item--active');
+        button.classList.add('tabs__button--active')
+        this.data[button.dataset.index].classList.add('tabs__item--active')
+
+        this.activeButton = button;
+        this.activeTab =   this.data[button.dataset.index]
+      }
+
+    })
+
   }
 }
 
-// подписка на скролл
-window.addEventListener('scroll', handleScroll)
+const tabs = new Tabs('.product__info');
+console.log(tabs)
